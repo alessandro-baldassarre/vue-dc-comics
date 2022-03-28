3cb7fbbfa898d7143f56139ab9693918c4fc9882
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-03-28 17:21:40 +0200

    realized Products component

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,17 +1,43 @@
 <template>
   <div>
-    <h1>Products</h1>
+    <div class="products">
+      <div class="container">
+        <h3>Content</h3>
+      </div>
+    </div>
+    <div class="menu-buy">
+      <div class="container"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexHeader',
-  
-}
+  name: "IndexProducts",
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.container {
+  width: 67%;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
+.products {
+  height: 100px;
+  background-color: rgb(26, 26, 26);
+  h3 {
+    color: white;
+  }
+}
+
+.menu-buy {
+  height: 110px;
+  background-color: rgb(0, 116, 245);
+}
 </style>
